d5127abaea8a96436c3ad19cd4c771afdc8a6f88
sangone88 <llil1@naver.com>
2019-10-31 22:09:41 +0900

    Intent 데이터 받아서 사용하기. (받는 입장)

--- app/src/main/java/com/tjoeun/a20191031_01_androidfunctions/MainActivity.kt
+++ app/src/main/java/com/tjoeun/a20191031_01_androidfunctions/MainActivity.kt
@@ -2,11 +2,19 @@ package com.tjoeun.a20191031_01_androidfunctions
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        intent에 첨부된 데이터를 찾아주는 코드.
+        var receivedName = intent.getStringExtra("name")
+        var receivedNumber = intent.getStringExtra("memberNumber")
+
+        memberNumTxt.text = "${receivedNumber} 번 회원"
+        memberNameTxt.text = receivedName
     }
 }
