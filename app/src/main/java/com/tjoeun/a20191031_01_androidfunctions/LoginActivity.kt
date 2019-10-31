d428947f14518392cd5ee15f70526bcfe7867d14
sangone88 <llil1@naver.com>
2019-10-31 21:56:28 +0900

    Intent 기초 활용 연습

--- app/src/main/java/com/tjoeun/a20191031_01_androidfunctions/LoginActivity.kt
+++ app/src/main/java/com/tjoeun/a20191031_01_androidfunctions/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.tjoeun.a20191031_01_androidfunctions
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -21,7 +22,22 @@ class LoginActivity : AppCompatActivity() {
 
 
             Toast.makeText(this, "${userNumber}번 ${userName} 회원님 환영합니다.", Toast.LENGTH_SHORT).show()
+
+            var myIntent = Intent(this, MainActivity::class.java)
+            startActivity(myIntent)
+
+        }
+
+        signUpBtn.setOnClickListener {
+            var signIntent = Intent(this, SignUpActivity::class.java)
+            startActivity(signIntent)
+
         }
 
     }
 }
+
+
+// 연습문제.
+// 회원가입 (SignUp) 화면을 만들고, 배경을 초록색 ( #00FF00 )으로만 설정하자.
+// 회원가입 버튼을 누르면 해당 액티비티로 이동하고록.
---
+++ app/src/main/java/com/tjoeun/a20191031_01_androidfunctions/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.tjoeun.a20191031_01_androidfunctions
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
